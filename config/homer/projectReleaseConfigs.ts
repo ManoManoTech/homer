49c0a59cff03a6cacf41b59868ee3364f4f81eb7
Pascal Fong Kye <pascal.fongkye@manomano.com>
2024-08-29 16:59:45 +0200

    feat(config): use json for project configs

--- config/homer/projectReleaseConfigs.ts
+++ config/homer/projectReleaseConfigs.ts
@@ -1,15 +1,48 @@
-import { SLACK_SUPPORT_CHANNEL_ID } from '@/constants';
 import { defaultReleaseManager } from '@/release/commands/create/managers/defaultReleaseManager';
+import { federationReleaseTagManager } from '@/release/commands/create/managers/federationReleaseTagManager';
+import { libraryReleaseManager } from '@/release/commands/create/managers/libraryReleaseManager';
+import { semanticReleaseTagManager } from '@/release/commands/create/managers/semanticReleaseTagManager';
 import { stableDateReleaseTagManager } from '@/release/commands/create/managers/stableDateReleaseTagManager';
 import type { ProjectReleaseConfig } from '@/release/typings/ProjectReleaseConfig';
+import type { ReleaseManager } from '@/release/typings/ReleaseManager';
+import type { ReleaseTagManager } from '@/release/typings/ReleaseTagManager';
+import projectsConfig from './projects.json';
 
-export const projectReleaseConfigs: ProjectReleaseConfig[] = [
-  // tools/homer
-  {
-    notificationChannelIds: [SLACK_SUPPORT_CHANNEL_ID],
-    projectId: 1148,
-    releaseChannelId: SLACK_SUPPORT_CHANNEL_ID,
-    releaseManager: defaultReleaseManager,
-    releaseTagManager: stableDateReleaseTagManager,
-  },
-];
+const releaseManagers: Record<string, ReleaseManager> = {
+  defaultReleaseManager,
+  libraryReleaseManager,
+};
+const releaseTagManagers: Record<string, ReleaseTagManager> = {
+  federationReleaseTagManager,
+  semanticReleaseTagManager,
+  stableDateReleaseTagManager,
+};
+
+function buildProjectReleaseConfigs(): ProjectReleaseConfig[] {
+  const projects = [];
+  for (const project of projectsConfig.projects) {
+    const {
+      releaseManager,
+      releaseTagManager,
+      notificationChannelIds,
+      projectId,
+      releaseChannelId,
+    } = project;
+    if (
+      releaseManager in releaseManagers &&
+      releaseTagManager in releaseTagManagers
+    ) {
+      projects.push({
+        notificationChannelIds,
+        projectId,
+        releaseChannelId,
+        releaseManager: releaseManagers[releaseManager],
+        releaseTagManager: releaseTagManagers[releaseTagManager],
+      });
+    }
+  }
+  return projects;
+}
+
+export const projectReleaseConfigs: ProjectReleaseConfig[] =
+  buildProjectReleaseConfigs();
