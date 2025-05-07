2dbf451123f268591fbe5a1041fd2a88f777186d
Thibaut FARCIN <thibaut.farcin@manomano.com>
2025-05-07 15:38:44 +0200

    feat(loading-release): Add a loading modal for the release creation to avoid potential expired trigger_id (3sec timeout from slack)

---
+++ src/release/commands/create/viewBuilders/buildReleaseModalLoadingView.ts
@@ -0,0 +1,26 @@
+import type { View } from '@slack/web-api';
+
+export async function buildReleaseModalLoadingView(): Promise<View> {
+  return {
+    type: 'modal',
+    callback_id: 'release-create-modal',
+    title: {
+      type: 'plain_text',
+      text: 'Release',
+    },
+    submit: {
+      type: 'plain_text',
+      text: 'Start',
+    },
+    notify_on_close: false,
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'plain_text',
+          text: ':loader: Loading projects data, please wait...',
+        },
+      },
+    ],
+  };
+}
